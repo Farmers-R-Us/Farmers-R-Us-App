7007c691c8f1e1390c5636b5ab296cd0cd42c62e
Benjamin Ly <benjaminly702@gmail.com>
2023-02-14 09:31:34 -0800

    Added create user backend

---
+++ server/controllers/userController.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+const db = require('../models/model.ts');
+
+interface controller {
+  get: (req: Request, res: Response, next: NextFunction) => void;
+  createUser: (req: Request, res: Response, next: NextFunction) => void;
+}
+
+const userController: controller = {
+  get: null,
+  createUser: null,
+};
+
+userController.createUser = (req, res, next) => {
+  const { gmail, isadmin, created_at, strikes, isblacklisted } = req.body;
+  console.log(gmail, isadmin, created_at, strikes, isblacklisted);
+  const queryString = `INSERT INTO users (gmail, isadmin, created_at, strikes, isblacklisted)
+        VALUES ('${gmail}','${isadmin}','${created_at}','${strikes}','${isblacklisted}')`;
+  db.query(queryString)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return next();
+};
+
+export { userController };
---
+++ server/routes/routes.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { userController } = require('../controllers/userController.ts');
+
+// CREATE USER
+router.post('/', userController.createUser, (req, res) => {
+  return res.status(200).json('Created User');
+});
+
+// INSERT INTO INVENTORY
+// router.post('/inventory', userController.insertInventory, (req, res) => {
+//   return res.status(200).json('Inserted into Inventory');
+// });
+
+export { router };
