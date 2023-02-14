a71e226d9f1236dacb1d812789cf52d3850647cf
Steven Tong <cytong@gmail.com>
2023-02-14 14:27:19 -0700

    frontend changes

--- client/components/Customer.tsx
+++ client/components/Customer.tsx
@@ -3,7 +3,25 @@ import React from 'react';
 export default function Customer() {
     return (
         <h3>
-            This is from customer
+            List of all available products 
         </h3>
+
+        // pull all available proudcts from db - need a GET endpoint
+
+        //<PaginatedProducts /> component:
+            // make it horizontally navigateable lol 
+                // <ProductCard /> component:
+                    // product image
+                    // product name 
+                    // product unit 
+                    // quantity 
+
+                // <AddToCart /> component:
+                    // button: add to cart
+                        // value field (num)
+            
+            // forward arrow (down-arrow on mobile) - get icons from react-icons or whatever that was 
+            // backward arrow (up-arrow on mobile)
+
     )
 }
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     return (
         <>
           <h2>hello world</h2>
-          
             <Header />
             {/* ternary */}
             <Customer />
