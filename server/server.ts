70a7e1cfe854e898dd53f898f2df0a4e8b87a5a3
StevenCYTong <113644724+StevenCYTong@users.noreply.github.com>
2023-02-14 15:03:46 -0700

    Revert "Backend"

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { router: routes } = require('./routes/routes.ts');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', routes);
-
-app.use('*', (req, res) => res.sendStatus(404));
-
-app.use((err, req, res, next) => {
-  const defaultError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'An error occurred' },
-  };
-  // console.log('before error: ', err);
-  const errorObj = Object.assign(defaultError, err);
-  console.log(errorObj.log);
-  //not sure if json is in the right place
-  return res.status(errorObj.status).send(JSON.stringify(errorObj.message));
-});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
