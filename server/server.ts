b160954248c478bf2e7b7ff83917464506c3a701
Benjamin Ly <benjaminly702@gmail.com>
2023-02-14 17:31:39 -0800

    Bug fixes for backend

--- server/server.ts
+++ server/server.ts
@@ -8,22 +8,29 @@ const { router: routes } = require('./routes/routes.ts');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.use('*', (req: Request, res: Response) => res.sendStatus(404));
 
-app.use((err:ErrorRequestHandler, req: Request, res:Response, next:NextFunction) => {
-  const defaultError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'An error occurred' },
-  };
-  // console.log('before error: ', err);
-  const errorObj = Object.assign(defaultError, err);
-  console.log(errorObj.log);
-  //not sure if json is in the right place
-  return res.status(errorObj.status).send(JSON.stringify(errorObj.message));
-});
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const defaultError = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    // console.log('before error: ', err);
+    const errorObj = Object.assign(defaultError, err);
+    console.log(errorObj.log);
+    //not sure if json is in the right place
+    return res.status(errorObj.status).send(JSON.stringify(errorObj.message));
+  }
+);
 
 app.listen(3000, () => console.log('Listening on port 3000'));
-export { app }
+export { app };
