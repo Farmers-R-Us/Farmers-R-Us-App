1319287c61f42e7a804e8a260ee24aeeaa9d13f6
Steven Tong <cytong@gmail.com>
2023-02-15 10:03:21 -0700

    admin update

--- client/components/Admin.tsx
+++ client/components/Admin.tsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import InventoryBox from './InventoryBox';
+
+export default function Admin() {
+
+    return (
+        <div className="adminPage">
+            <h3 className="adminTitle">View Your Inventory</h3>
+            <div className="inventoryList">
+                <InventoryBox />
+            </div>
+        </div>
+    )
+}
